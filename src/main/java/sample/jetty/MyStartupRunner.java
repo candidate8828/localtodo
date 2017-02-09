a9345829d5b62cded623d3059d259692aa424b26
gfyangsong <gfyangsong@gf.com.cn>
2017-02-09 15:24:18 +0800

    1

--- src/main/java/sample/jetty/MyStartupRunner.java
+++ src/main/java/sample/jetty/MyStartupRunner.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package sample.jetty;
 
 import org.springframework.boot.CommandLineRunner;
@@ -11,17 +10,3 @@ public class MyStartupRunner implements CommandLineRunner {
         System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");
     }
 }
-=======
-package sample.jetty;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyStartupRunner implements CommandLineRunner {
-	@Override
-    public void run(String... args) throws Exception {
-        System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");
-    }
-}
->>>>>>> branch 'master' of https://github.com/candidate8828/localtodo.git
