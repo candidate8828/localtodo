19809d95834958af136875ba44610fcef9999902
Anson <liangyifang@foxmail.com>
2017-02-09 15:55:56 +0800

    1

--- src/main/java/sample/jetty/MyStartupRunner.java
+++ src/main/java/sample/jetty/MyStartupRunner.java
@@ -9,4 +9,4 @@ public class MyStartupRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");
     }
-}
+} 
